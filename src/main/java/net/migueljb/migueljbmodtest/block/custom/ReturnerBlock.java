4df50e7e05ce4aa8f230e94f58a3c40f14ea6fd3
migjimbar1 <migueljb2000@gmail.com>
2025-02-18 17:43:34 +0100

    added blank calss for ReturnerBlock and png

---
+++ src/main/java/net/migueljb/migueljbmodtest/block/custom/ReturnerBlock.java
@@ -0,0 +1,10 @@
+package net.migueljb.migueljbmodtest.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class ReturnerBlock extends Block {
+    public ReturnerBlock(Properties properties) {
+        super(properties);
+    }
+
+}
